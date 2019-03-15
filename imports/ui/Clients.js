b0a45bb0cf5d2cf173c1de19c96663ff1b50a84a
SimasDei <simondeiman@gmail.com>
2019-03-15 16:07:08 +0200

    add React, React Router. Setup Basic Project Structure

---
+++ imports/ui/Clients.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Clients extends Component {
+  render() {
+    return (
+      <div>
+        <p>Clients Component</p>
+      </div>
+    );
+  }
+}
+
+export default Clients;
---
+++ imports/ui/Products.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Products extends Component {
+  render() {
+    return (
+      <div>
+        <p>Products Component</p>
+      </div>
+    );
+  }
+}
+
+export default Products;
